f276ab9de23bf2cf11d8a28e5258823d5a663f4c
Chong <ch0ngx1an@hotmail.com>
2022-08-20 15:16:36 +0800

    initialise list products page

--- src/pages/products/index.vue
+++ src/pages/products/index.vue
@@ -5,8 +5,33 @@ import Layout from '../../layouts/Layout.vue';
 
 <template>
   <Layout>
-    <div>
-      List products
+    <div class="m-5">
+      <h1 class="text-3xl font-bold">Products</h1>
+      <div class="mt-10 p-3 border rounded border-gray-100">
+        <table class="text-left w-full">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>T-shirt</td>
+              <td>$99.99</td>
+            </tr>
+            <tr>
+              <td>Jacket</td>
+              <td>$159.99</td>
+            </tr>
+            <tr>
+              <td>Pants</td>
+              <td>$109.99</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+
   </Layout>
 </template>
